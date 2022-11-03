015abde9bb704b8a257d983b78201f4c7f1ee4f4
Wickhye <jihyesim170@gmail.com>
2022-11-03 20:40:04 +0900

    5 0 Introdution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Button from './Button';
-import styled from "./App.module.css";
+import { useState } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+    console.log("render");
+  }
   return (
     <div>
-      <h1 className={styled.title}>Welcome back!</h1>
-      <Button text={"Countinue"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
