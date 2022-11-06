57d901da9f44f0df967261f88032f179616e9d99
Wickhye <jihyesim170@gmail.com>
2022-11-06 22:17:49 +0900

    5 3 Recap

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState(""); // keyword는 setKeyword에 의해 변화됨
   const onChange = (event) => setKeyword(event.target.value);
   const onClick = () => {
-    setValue((prev) => prev + 1);
+    setKeyword((prev) => prev + 1); // setKeyword는 onChange란 function에 의해 호출
     console.log("i run all the time");
   }
   useEffect( () =>{
-    console.log("i run only once.");
-  }, []);
+    console.log("i run only once."); // 우리가 실행시키고 싶은 코드, dependencies(react.js가 지켜보아야 하는 것들)
+  }, []); // 처음 렌더 시에 딱 한번만 실행
   useEffect( () =>{
     console.log("i run when 'keyword' changes.");
-  }, [keyword]);
+  }, [keyword]); // 시작할 때와 keyword 변화 시에 실행
   useEffect(()=> {
     console.log("i run when 'counter' changes.");
-  }, [counter]);
+  }, [counter]); // 시작할 때와 counter 변화 시에 실행
   useEffect(()=> {
     console.log("i run when keyword & counter changes.");
-  }, [keyword, counter]);
+  }, [keyword, counter]); // 시작할 때와 counter, keyword 둘 중 하나가 변화 시에 실행
   return (
     <div>
       <input 
         value={keyword}
-        onChange={onChange}
+        onChange={onChange} // onChange는 사용자의 input이 변화할 때 호출
         type="text"
         placeholder="Search here..."/>
       <h1>{counter}</h1>
