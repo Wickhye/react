205cfc1d4aae8d5e86e5f40ac515f299e584e1f3
Wickhye <jihyesim170@gmail.com>
2022-11-15 22:30:39 +0900

    6 6 Parameters

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    console.log(json.data.movie);
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading Detail</h1>
+      ) : (
+        <div>
+          <img src={movie.large_cover_image} />
+          <h2>{movie.title_long}</h2>
+          <p>{movie.date_uploaded}</p>
+          <p>Like count: {movie.like_count}</p>
+          <p>Runtime: {movie.runtime}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
